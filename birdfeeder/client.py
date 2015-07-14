6f5619edf460b6ccad2580c13d02a7710ffee8e4
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-07-14 18:25:11 +0200

    working on crawling thredds ...

--- birdfeeder/client.py
+++ birdfeeder/client.py
@@ -5,8 +5,17 @@ logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.WARN)
 logger = logging.getLogger(__name__)
 
 
-def feed(thredds_url, solr_url):
-    pass
+def feed(url, solr_url):
+    import pysolr
+    from threddsclient import client as tdsclient
+    logger.info("tds=%s, solr=%s", url, solr_url)
+    solr = pysolr.Solr(solr_url, timeout=10)
+    ## results = solr.search('bananas')
+    ## for result in results:
+    ##     print "The title is '{0}'.".format(result['title'])
+
+    for ds in tdsclient.crawl(url, depth=1):
+        logger.info(ds.name)
 
 def create_parser():
     import argparse
@@ -16,21 +25,24 @@ def create_parser():
         usage='''birdfeeder [<options>]''',
         description="Feed solr from thredds",
         )
-    parser.add_argument("--thredds-url",
-                        dest='thredds_url',
+    parser.add_argument("--catalog-url",
+                        dest='catalog_url',
                         required=True,
+                        type=type(''),
                         help="Thredds Catalog URL",
                         action="store")
     parser.add_argument("--solr-url",
                         dest='solr_url',
-                        required=True,
+                        required=False,
+                        type=type(''),
+                        default='http://localhost:8983/solr/',
                         help="Solr URL",
                         action="store")
     return parser
 
 
 def execute(args):
-    return feed(args.thredds_url, args.solr_url)
+    return feed(args.catalog_url, args.solr_url)
 
 def main():
     import argcomplete
