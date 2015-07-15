79ddd4411ff2de3d435ca58b156d0295fae46d25
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-07-15 16:18:18 +0200

    fill solr fields from thredds ...

--- birdfeeder/client.py
+++ birdfeeder/client.py
@@ -4,16 +4,22 @@ logger = logging.getLogger(__name__)
 
 def feed(url, solr_url):
     import pysolr
-    from threddsclient import client as tdsclient
+    import threddsclient
     logger.info("tds=%s, solr=%s", url, solr_url)
     solr = pysolr.Solr(solr_url, timeout=10)
-    ## results = solr.search('bananas')
-    ## for result in results:
-    ##     print "The title is '{0}'.".format(result['title'])
 
-    for ds in tdsclient.crawl(url, depth=1):
+    for ds in threddsclient.crawl(url, depth=1):
         logger.info(ds.name)
-        solr.add([ dict(id=ds.ID, title=ds.name) ])
+        solr.add([ dict(
+            id=ds.ID,
+            title=ds.name,
+            content_type=ds.content_type,
+            last_modified=ds.modified,
+            resourcename=ds.ID,
+            url=ds.download_url(),
+            opendap_url=ds.opendap_url(),
+            wms_url=ds.wms_url(),
+            catalog_url=ds.url)])
 
 
 
