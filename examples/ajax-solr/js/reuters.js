1efc5a8c852812c8adc10c544c0ee7965e1fa4a2
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-07-16 18:04:10 +0200

    ajax-solr example added

---
+++ examples/ajax-solr/js/reuters.js
@@ -0,0 +1,87 @@
+var Manager;
+
+require.config({
+  paths: {
+    core: '../../../core',
+    managers: '../../../managers',
+    widgets: '../../../widgets',
+    reuters: '../widgets'
+  },
+  urlArgs: "bust=" +  (new Date()).getTime()
+});
+
+(function ($) {
+
+define([
+  'managers/Manager.jquery',
+  'core/ParameterStore',
+  'reuters/ResultWidget',
+  'reuters/TagcloudWidget',
+  'reuters/CurrentSearchWidget.9',
+  'reuters/AutocompleteWidget',
+  'reuters/CountryCodeWidget',
+  'reuters/CalendarWidget',
+  'widgets/jquery/PagerWidget'
+], function () {
+  $(function () {
+    Manager = new AjaxSolr.Manager({
+      solrUrl: 'http://localhost:8983/solr/birdhouse/'
+    });
+    Manager.addWidget(new AjaxSolr.ResultWidget({
+      id: 'result',
+      target: '#docs'
+    }));
+    Manager.addWidget(new AjaxSolr.PagerWidget({
+      id: 'pager',
+      target: '#pager',
+      prevLabel: '&lt;',
+      nextLabel: '&gt;',
+      innerWindow: 1,
+      renderHeader: function (perPage, offset, total) {
+        $('#pager-header').html($('<span></span>').text('displaying ' + Math.min(total, offset + 1) + ' to ' + Math.min(total, offset + perPage) + ' of ' + total));
+      }
+    }));
+    var fields = [ 'title', 'variable' ];
+    for (var i = 0, l = fields.length; i < l; i++) {
+      Manager.addWidget(new AjaxSolr.TagcloudWidget({
+        id: fields[i],
+        target: '#' + fields[i],
+        field: fields[i]
+      }));
+    }
+    Manager.addWidget(new AjaxSolr.CurrentSearchWidget({
+      id: 'currentsearch',
+      target: '#selection'
+    }));
+    Manager.addWidget(new AjaxSolr.AutocompleteWidget({
+      id: 'text',
+      target: '#search',
+      fields: [ 'title', 'variable' ]
+    }));
+    Manager.init();
+    Manager.store.addByValue('q', '*:*');
+    var params = {
+      facet: true,
+      'facet.field': [ 'title', 'variable' ],
+      'facet.limit': 20,
+      'facet.mincount': 1,
+      'f.topics.facet.limit': 50,
+      'json.nl': 'map'
+    };
+    for (var name in params) {
+      Manager.store.addByValue(name, params[name]);
+    }
+    Manager.doRequest();
+  });
+
+  $.fn.showIf = function (condition) {
+    if (condition) {
+      return this.show();
+    }
+    else {
+      return this.hide();
+    }
+  }
+});
+
+})(jQuery);
