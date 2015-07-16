543db8c29437e47d264c29aa28ada31073f602f4
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-07-16 18:19:08 +0200

    revert ajax-solr example to original

--- examples/ajax-solr/js/reuters.js
+++ examples/ajax-solr/js/reuters.js
@@ -25,7 +25,7 @@ define([
 ], function () {
   $(function () {
     Manager = new AjaxSolr.Manager({
-      solrUrl: 'http://localhost:8983/solr/birdhouse/'
+      solrUrl: 'http://reuters-demo.tree.ewdev.ca:9090/reuters/'
     });
     Manager.addWidget(new AjaxSolr.ResultWidget({
       id: 'result',
@@ -41,7 +41,7 @@ define([
         $('#pager-header').html($('<span></span>').text('displaying ' + Math.min(total, offset + 1) + ' to ' + Math.min(total, offset + perPage) + ' of ' + total));
       }
     }));
-    var fields = [ 'title', 'variable' ];
+    var fields = [ 'topics', 'organisations', 'exchanges' ];
     for (var i = 0, l = fields.length; i < l; i++) {
       Manager.addWidget(new AjaxSolr.TagcloudWidget({
         id: fields[i],
@@ -56,16 +56,31 @@ define([
     Manager.addWidget(new AjaxSolr.AutocompleteWidget({
       id: 'text',
       target: '#search',
-      fields: [ 'title', 'variable' ]
+      fields: [ 'topics', 'organisations', 'exchanges' ]
+    }));
+    Manager.addWidget(new AjaxSolr.CountryCodeWidget({
+      id: 'countries',
+      target: '#countries',
+      field: 'countryCodes'
+    }));
+    Manager.addWidget(new AjaxSolr.CalendarWidget({
+      id: 'calendar',
+      target: '#calendar',
+      field: 'date'
     }));
     Manager.init();
     Manager.store.addByValue('q', '*:*');
     var params = {
       facet: true,
-      'facet.field': [ 'title', 'variable' ],
+      'facet.field': [ 'topics', 'organisations', 'exchanges', 'countryCodes' ],
       'facet.limit': 20,
       'facet.mincount': 1,
       'f.topics.facet.limit': 50,
+      'f.countryCodes.facet.limit': -1,
+      'facet.date': 'date',
+      'facet.date.start': '1987-02-26T00:00:00.000Z/DAY',
+      'facet.date.end': '1987-10-20T00:00:00.000Z/DAY+1DAY',
+      'facet.date.gap': '+1DAY',
       'json.nl': 'map'
     };
     for (var name in params) {
