5f5d4a0329a87d3d5397b1a4633265861a4656ff
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-02-09 15:23:56 +0100

    update spider writer test

--- tests/test_spider.py
+++ tests/test_spider.py
@@ -21,4 +21,4 @@ def test_crawl_dmi_2():
 @attr('online')
 def test_write_datasets():
     page="http://ensemblesrt3.dmi.dk/data/CORDEX/AFR-44/KNMI/MOHC-HadGEM2-ES/rcp45/r1i1p1/KNMI-RACMO22T/v1/mon/tasmax/"
-    spider.write_datasets(page, depth=1)
+    spider.write_datasets(page, depth=1, filename="/tmp/out.csv")
